e16f2b5edd44786b4cc0cd72592e124c3561157f
alkathirikhalid <khalid@alkathirikhalid.com>
2024-11-01 18:01:20 +0800

    added glide util

---
+++ app/src/main/java/com/alkathirikhalid/userposts/util/Util.kt
@@ -0,0 +1,38 @@
+package com.alkathirikhalid.userposts.util
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.alkathirikhalid.userposts.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+/**
+ *
+ * @author alkathirikhalid
+ * @project User Posts
+ * @date 01/11/2024
+ */
+private fun getProgressDrawable(context: Context): CircularProgressDrawable {
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = 10f
+        centerRadius = 50f
+        start()
+    }
+}
+
+private fun ImageView.loadImage(uri: String?, progressDrawable: CircularProgressDrawable) {
+    val options = RequestOptions()
+        .placeholder(progressDrawable)
+        .error(R.mipmap.ic_launcher)
+    Glide.with(context)
+        .setDefaultRequestOptions(options)
+        .load(uri)
+        .into(this)
+}
+
+@BindingAdapter("android:imageUrl")
+fun loadImage(view: ImageView, url: String?) {
+    view.loadImage(url, getProgressDrawable(view.context))
+}
