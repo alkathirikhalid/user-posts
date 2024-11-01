5520fae25a9f4fc24bd01dd07bcd58d509f161f5
alkathirikhalid <khalid@alkathirikhalid.com>
2024-11-01 16:17:34 +0800

    set up navigation and action bar

--- app/src/main/java/com/alkathirikhalid/userposts/view/MainActivity.kt
+++ app/src/main/java/com/alkathirikhalid/userposts/view/MainActivity.kt
@@ -2,16 +2,22 @@ package com.alkathirikhalid.userposts.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.alkathirikhalid.userposts.R
-import com.alkathirikhalid.userposts.api.UserPostAPIService
 
 class MainActivity : AppCompatActivity() {
-    private val userPostAPIService = UserPostAPIService()
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = Navigation.findNavController(this, R.id.fragmentContainerView)
+        NavigationUI.setupActionBarWithNavController(this, navController)
 
-        userPostAPIService.getUsers() // test log
+    }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
