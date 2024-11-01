5246a6eb14f7a5cef4c128c326d669eb12540b01
alkathirikhalid <khalid@alkathirikhalid.com>
2024-11-01 13:36:47 +0800

    added base view model and manifest internet permission

---
+++ app/src/main/java/com/alkathirikhalid/userposts/viewmodel/BaseViewModel.kt
@@ -0,0 +1,28 @@
+package com.alkathirikhalid.userposts.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
+/**
+ *
+ * @author alkathirikhalid
+ * @project User Posts
+ * @date 01/11/2024
+ */
+abstract class BaseViewModel(application: Application) : AndroidViewModel(application),
+    CoroutineScope {
+
+    private val job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
+}
